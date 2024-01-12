4c1ed6f6ed23fa598ba887e87f42eb616a8e2cee
ProjectSoft-STUDIONIONS <projectsoft2009@yandex.ru>
2024-01-12 09:00:01 +0400

    update config ClientSettings

---
+++ site/assets/modules/clientsettings/config/tab100.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+	'caption' => 'SEO',
+	'introtext' => 'SEO (Yandex, Google, etc..)',
+	//'roles' => [1], // admin only
+	'settings' => [
+		'yandex_verification' => [
+			'caption' => 'Yandex Verification',
+			'type'  => 'text',
+			'note'  => 'Код верификации Yandex',
+			'default_text' => '',
+		],
+	],
+];
